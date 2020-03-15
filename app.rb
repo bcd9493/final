eab0aab1ca2acf3651b3bd67d2b7bbd29194c91e
bcd9493 <brad.dominy@kellogg.northwestern.edu>
2020-03-15 03:37:26 +0000

    Start

--- app.rb
+++ app.rb
@@ -14,5 +14,31 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
-events_table = DB.from(:events)
-rsvps_table = DB.from(:rsvps)
+shops_table = DB.from(:shops)
+rankings_table = DB.from(:rankings)
+
+get "/" do
+    puts shops_table.all
+    @shops = shops_table.all
+    view "shops"
+end
+
+get "/shops/:id" do
+    @shop = shops_table.where(id: params[:id]).first
+    view "shop"
+end
+
+get "/shops/:id/rankings/new" do
+    @shop = shops_table.where(id: params[:id]).first
+    view "shop_rank"
+end
+
+get "/shops/:id/rankings/ranked" do
+    puts params
+    @shop = shops_table.where(id: params["id"])
+    rankings_table.insert(event_id: params["id"],
+                       user_id: session["user_id"],
+                       going: params["going"],
+                       comments: params["comments"])
+    view "ranked"
+end
--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,37 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :shops do
   primary_key :id
   String :title
   String :description, text: true
   String :date
   String :location
 end
-DB.create_table! :rsvps do
+DB.create_table! :rankings do
   primary_key :id
   foreign_key :event_id
+  foreign_key :user_id
   Boolean :going
-  String :name
-  String :email
   String :comments, text: true
 end
+DB.create_table! :users do
+  primary_key :id
+  String :name
+  String :email
+  String :password
+end
+
 
 # Insert initial (seed) data
-events_table = DB.from(:events)
+shops_table = DB.from(:shops)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
+shops_table.insert(title: "Bacon Burger Taco Fest", 
                     description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
                     date: "June 21",
                     location: "Kellogg Global Hub")
 
-events_table.insert(title: "Kaleapolooza", 
+shops_table.insert(title: "Kaleapolooza", 
                     description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
                     date: "July 4",
                     location: "Nowhere")
